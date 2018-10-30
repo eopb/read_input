820533231fc8da2181ce13f0eb869b13b11423b7
Ethan Brierley <ethanboxx@gmail.com>
2018-10-30 11:10:10 +0000

    simplify

--- examples/main.rs
+++ examples/main.rs
@@ -5,10 +5,7 @@ extern crate read_input;
 use read_input::*;
 
 fn main() {
-    println!(
-        "output {}",
-        String::input_new().default("fatpeepople".to_string()).get()
-    );
+    println!("output {}", String::input_new().get());
     println!("output {}", i32::simple_input());
     println!("output {}", i32::valid_input(&|x: &i32| 4 < *x && *x < 9));
     println!("output {}", char::simple_input());
