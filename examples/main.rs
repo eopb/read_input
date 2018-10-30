40e21ea08f5542f4ad9c8ca37cf85f5993a81c33
Ethan Brierley <ethanboxx@gmail.com>
2018-10-30 09:59:28 +0000

    not very good but now it works

--- examples/main.rs
+++ examples/main.rs
@@ -10,6 +10,6 @@ fn main() {
         u64::input_new().default(32).test(&|v| v > &10).get()
     );
     println!("output {}", i32::simple_input());
-    // println!("output {}", i32::valid_input(|x| 4 < *x && *x < 9));
+    println!("output {}", i32::valid_input(&|x: &i32| 4 < *x && *x < 9));
     println!("output {}", char::simple_input());
 }
