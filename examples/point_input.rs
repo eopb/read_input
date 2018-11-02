0a8e5fb14690c23bda134162696e8bc38ecdea15
Ethan Brierley <ethanboxx@gmail.com>
2018-11-02 14:14:31 +0000

    add program spec

---
+++ examples/point_input.rs
@@ -0,0 +1,4 @@
+//To run this example `cargo run --example point_input --release`
+//Example program that allows a user to input a point in 2D space.
+//This program adapted from the `std::str::FromStr` trait documentation example.
+//The porgram was writen to show the use of the `err_match()` method in `read_input`
