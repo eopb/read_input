97f5f5531562cb2420905939f11ca1973f24392b
Ethan Brierley <ethanboxx@gmail.com>
2018-11-25 18:41:02 +0000

    update for new system

--- examples/url.rs
+++ examples/url.rs
@@ -1,8 +1,11 @@
+extern crate dont_disappear;
 extern crate read_input;
 extern crate url;
+
 use read_input::*;
 use std::error::Error;
 use url::Url;
+
 fn main() {
     println!(
         "You inputted the URL {:#?}",
@@ -11,4 +14,5 @@ fn main() {
             .err_match(|e| Some(format!("Error \"{}\"", e.description())))
             .get()
     );
+    dont_disappear::enter_to_continue::default();
 }
