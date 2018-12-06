85a73f3bf3c33d1fabc2a85f3a364f852f273a11
Ethan Brierley <ethanboxx@gmail.com>
2018-12-06 09:24:48 +0000

    print useful examples in default

--- examples/default.rs
+++ examples/default.rs
@@ -6,11 +6,15 @@ extern crate read_input;
 use read_input::prelude::shortcut::default_input_set;
 
 fn main() {
+    println!("Type i32");
     default_input_set::<i32>().get();
+    println!("Type u32");
     default_input_set::<u32>().get();
+    println!("Type f32");
     default_input_set::<f32>().get();
+    println!("Type bool");
     default_input_set::<bool>().get();
+    println!("Type char");
     default_input_set::<char>().get();
-
     dont_disappear::enter_to_continue::default();
 }
