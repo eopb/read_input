57afe98997ca0a358c0192cc55b866633668b3d3
Ethan Brierley <ethanboxx@gmail.com>
2018-12-23 10:26:02 +0000

    support `Vec<T>` and `[T]` in is_in

--- src/lib.rs
+++ src/lib.rs
@@ -336,7 +336,7 @@ where
         InputBuilder {
             test: {
                 let mut x = self.test;
-                x.push((is.contains(), None));
+                x.push((is.contains_func(), None));
                 x
             },
             ..self
@@ -359,41 +359,59 @@ where
 }
 
 trait IsInFunc<T> {
-    fn contains(self) -> Box<Fn(&T) -> bool>;
+    fn contains_func(self) -> Box<Fn(&T) -> bool>;
 }
 
 impl<T: PartialOrd + 'static> IsInFunc<T> for Range<T> {
-    fn contains(self) -> Box<Fn(&T) -> bool> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
         Box::new(move |x| &self.start <= x && x < &self.end)
     }
 }
 
 impl<T: PartialOrd + 'static> IsInFunc<T> for RangeInclusive<T> {
-    fn contains(self) -> Box<Fn(&T) -> bool> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
         Box::new(move |x| self.start() <= x && x <= self.end())
     }
 }
 
 impl<T: PartialOrd + 'static> IsInFunc<T> for RangeFrom<T> {
-    fn contains(self) -> Box<Fn(&T) -> bool> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
         Box::new(move |x| &self.start <= x)
     }
 }
 
 impl<T: PartialOrd + 'static> IsInFunc<T> for RangeTo<T> {
-    fn contains(self) -> Box<Fn(&T) -> bool> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
         Box::new(move |x| &self.end > x)
     }
 }
 
 impl<T: PartialOrd + 'static> IsInFunc<T> for RangeToInclusive<T> {
-    fn contains(self) -> Box<Fn(&T) -> bool> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
         Box::new(move |x| &self.end >= x)
     }
 }
 
+impl<T: PartialEq + 'static> IsInFunc<T> for Vec<T> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
+        Box::new(move |x| self.contains(x))
+    }
+}
+
+impl<T> IsInFunc<T> for [T]
+where
+    Self: Sized,
+    T: PartialEq,
+    T: 'static,
+    T: Sized,
+{
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
+        Box::new(move |x| self.contains(x))
+    }
+}
+
 impl<T: 'static, F: Fn(&T) -> bool + 'static> IsInFunc<T> for F {
-    fn contains(self) -> Box<Fn(&T) -> bool> {
+    fn contains_func(self) -> Box<Fn(&T) -> bool> {
         Box::new(self)
     }
 }
