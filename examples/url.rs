c69a11db5b6d8f0edec95c2fbdbff64b605131af
Ethan Brierley <ethanboxx@gmail.com>
2020-02-24 19:06:20 +0000

    fix url example

--- examples/url.rs
+++ examples/url.rs
@@ -1,14 +1,14 @@
 //To run this example `cargo run --example url --release`
 
 use read_input::prelude::*;
-use read_input::shortcut::with_description;
+use read_input::shortcut::with_display;
 use url::Url;
 
 fn main() {
     println!(
         "You inputted the URL {:#?}",
         input::<Url>()
-            .err_match(with_description)
+            .err_match(with_display)
             .repeat_msg("Please input a URL: ")
             .get()
     );
